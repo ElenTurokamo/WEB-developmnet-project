b52b026231cd6a9898876db3e5c6cadd87e32361
ElenTurokamo <ElenNightForewer@gmail.com>
2025-10-17 14:07:39 +0500

    Фиксы стилей

--- js/parallax-scroll.js
+++ js/parallax-scroll.js
@@ -1,7 +1,7 @@
-function parallaxScroll() { // Функция для создания эффекта параллакса при прокрутке страницы.
-    const header = document.querySelector('body'); // Получаем элемент <header>
-    const scrolled = window.pageYOffset; // Определяем, сколько пикселей пользователь прокрутил страницу по вертикали
-    header.style.backgroundPositionY = -(scrolled * 0.3) + 'px'; // Изменяем вертикальную позицию фона хедера для создания эффекта параллакса
+function parallaxScroll() {
+  const body = document.querySelector('body'); // Получаем элемент body
+  const scrolled = window.scrollY; // сколько пикселей прокрутили
+  body.style.backgroundPositionY = -(scrolled * 0.3) + 'px'; // создаём смещение
 }
 
-window.addEventListener('scroll', parallaxScroll); // Добавляем событие прокрутки, которое вызывает функцию parallaxScroll при скроллинге страницы
+window.addEventListener('scroll', parallaxScroll);
