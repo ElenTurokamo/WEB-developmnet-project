41f87f87fc63cebdf3282060ea184c2188e69dcd
ElenTurokamo <ElenNightForewer@gmail.com>
2025-10-31 14:04:22 +0500

    Фиксы тёмной темы

--- js/dark-mode-index.js
+++ js/dark-mode-index.js
@@ -1,13 +1,12 @@
 function toggleDarkMode() {
-  var placeholderTop = document.querySelector('.placeholder-top');
+  const body = document.body;
+  const placeholderImage = document.querySelector('.decorative-placeholder img');
 
-  placeholderTop.classList.toggle('dark-theme');
+  body.classList.toggle('dark-theme');
 
-  if (placeholderTop.classList.contains('dark-theme')) {
-    document.querySelector('.theme-toggle').innerText = 'Светлая тема';
-    placeholderTop.style.background = "linear-gradient(180deg, rgba(135, 195, 255, 0.4), rgba(192, 230, 255, 0.25))";
+  if (body.classList.contains('dark-theme')) {
+    placeholderImage.src = "/img/misc/Deco-block-dark-theme.png";
   } else {
-    document.querySelector('.theme-toggle').innerText = 'Темная тема';
-    placeholderTop.style.background = "linear-gradient(180deg, rgba(255, 135, 135, 0.4), rgba(255, 192, 192, 0.25))";
+    placeholderImage.src = "/img/misc/Deco-block.png";
   }
 }
