2e1c1b76dbf9f453174d559466618ea26e0b8835
ElenTurokamo <117432225+ElenTurokamo@users.noreply.github.com>
2025-11-02 11:45:45 +0500

    Фиксы css

--- js/deco-block.js
+++ js/deco-block.js
@@ -5,11 +5,18 @@ container.addEventListener('mousemove', (e) => {
   const rect = container.getBoundingClientRect();
   const x = e.clientX - rect.left;
   const y = e.clientY - rect.top;
+  
   const rotateX = ((y / rect.height) - 0.5) * -25;
   const rotateY = ((x / rect.width) - 0.5) * 25;
-  img.style.transform = `translateX(50%) rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.05)`;
+
+  // просто меняем значения CSS-переменных
+  img.style.setProperty('--rotateX', `${rotateX}deg`);
+  img.style.setProperty('--rotateY', `${rotateY}deg`);
+  img.style.setProperty('--scale', `1.05`);
 });
 
 container.addEventListener('mouseleave', () => {
-  img.style.transform = 'translateX(50%) rotateX(0deg) rotateY(0deg) scale(1)';
+  img.style.setProperty('--rotateX', `0deg`);
+  img.style.setProperty('--rotateY', `0deg`);
+  img.style.setProperty('--scale', `1`);
 });
